0d22fa33fc11c537285fa58e755d86b5dd1d5815
mildfire <mildfire@gmail.com>
2023-11-08 22:51:27 +0500

    login & logout

--- pages/login.js
+++ pages/login.js
@@ -1,38 +1,26 @@
 import supabase from "@/utils/supabase";
 import styles from "../styles/Home.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
-export default function LoginPage() {
-  const [currentSession, setCurrentSession] = useState(null);
-  const [currentUser, setCurrentUser] = useState(null);
+function Login() {
+  const router = useRouter();
 
   async function handleSubmit(event) {
     event.preventDefault();
+
     const email = event.target.email.value;
-    // console.log(email);
-    // await supabase.auth.signIn({ email });
-    // await supabase.auth.signInWithPassword()
+    const password = event.target.password.value;
 
     const { data, error } = await supabase.auth.signInWithPassword({
       email: email,
-      password: "0892387639",
+      password: password,
     });
 
-    // console.log(data);
-    // console.log(error);
+    console.log("data from login ... ", data);
+    console.log("error from login ...", error);
 
-    // const user = JSON.stringify(await supabase.auth.getUser(), null, 2);
-    // const session = JSON.stringify(await supabase.auth.getSession(), null, 2);
-    const user = await supabase.auth.getUser();
-    const session = await supabase.auth.getSession();
-    setCurrentSession(session);
-    setCurrentUser(user);
-
-    // console.log(supabase.auth.user());
-    // console.log(user);
-    // console.log(session);
-
-    console.log(currentSession);
+    router.reload();
   }
 
   return (
@@ -46,16 +34,91 @@ export default function LoginPage() {
           name="email"
           defaultValue="mildfirey@yandex.ru"
         />
+        <label htmlFor="password"> Password </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          defaultValue="0892387639"
+        />
         <button type="submit"> Log In </button>
       </form>
-      <div>
-        {currentSession && (
-          <div>
-            <p>you are logged in as {currentSession.data.session.user.email}</p>
-            <pre> {JSON.stringify(currentUser.data.user, null, 2)} </pre>\
-          </div>
-        )}
-      </div>
+      {/* <pre> {JSON.stringify(session, null, 2)} </pre> */}
     </div>
   );
 }
+
+function SessionInfo({ session }) {
+  // console.log("session is...", session);
+
+  const router = useRouter();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const { error } = await supabase.auth.signOut();
+
+    console.log("Logging out ...  ", error);
+
+    router.reload();
+  }
+
+  return (
+    // <div className={styles.container}>{JSON.stringify(session, null, 2)}</div>
+    <div className={styles.container}>
+      {" "}
+      currently logged in as {session.user.email}
+      {/* <h1> Log Out </h1> */}
+      <form onSubmit={handleSubmit}>
+        <button type="submit"> Log Out </button>
+      </form>
+    </div>
+  );
+}
+
+export default function LoginPage() {
+  const [session, setSession] = useState(null);
+  const [user, setUser] = useState(null);
+
+  const get_session = async () => {
+    try {
+      const { data, error } = await supabase.auth.getSession();
+      if (error) {
+        console.error(error);
+      } else {
+        setSession(data.session);
+        setUser(data.session.user);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    get_session();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <h1>Session</h1>
+      {/* <pre> {JSON.stringify(session, null, 2)} </pre> */}
+
+      {session ? <SessionInfo session={session} /> : <Login />}
+
+      {/* {!session && <Login />} */}
+    </div>
+  );
+}
+
+// <div className={styles.container}>
+//   <div>
+//     {session ? (
+//       <div>
+//         <p>you are logged in as {session.data.session.user.email}</p>
+//         <pre> {JSON.stringify(user.data.user, null, 2)} </pre>\
+//       </div>
+//     ) : (
+//       <Login />
+//     )}
+//   </div>
+// </div>
