70a478867a9c70fc9926400f8b1121073f96d007
mildfire <mildfire@gmail.com>
2023-11-08 23:11:16 +0500

    auth

--- pages/titles/[title].js
+++ pages/titles/[title].js
@@ -1,7 +1,10 @@
 import supabase from "@/utils/supabase";
 import styles from "../../styles/Home.module.css";
+import { useState, useEffect } from "react";
 
 export async function getServerSideProps({ params }) {
+  // const { sessionData, sessionError } = await supabase.auth.getSession();
+
   const { data: products, error } = await supabase
     .from("Products")
     .select(
@@ -26,6 +29,8 @@ export async function getServerSideProps({ params }) {
     throw new Error(error);
   }
 
+  // console.log(await sessionData);
+
   return {
     props: {
       products,
@@ -34,9 +39,31 @@ export async function getServerSideProps({ params }) {
 }
 
 export default function ProductPage({ products }) {
+  const [session, setSession] = useState(null);
+  const [user, setUser] = useState(null);
+
+  const get_session = async () => {
+    try {
+      const { data, error } = await supabase.auth.getSession();
+      if (error) {
+        console.error(error);
+      } else {
+        setSession(data.session);
+        setUser(data.session.user);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    get_session();
+  }, []);
+
   // console.log(products);
   return (
     <div className={styles.container}>
+      <pre> {JSON.stringify(session, null, 2)} </pre>
       <pre> {JSON.stringify(products, null, 2)} </pre>
     </div>
   );
