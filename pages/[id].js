5e2850a93168bca51c757b64fa83b0641589e2ce
mildfire <mildfire@gmail.com>
2023-11-13 20:56:02 +0500

    single prod by id

--- pages/[id].js
+++ pages/[id].js
@@ -1,48 +1,91 @@
 import supabase from "@/utils/supabase";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 export async function getServerSideProps({ params }) {
-  const { data: product, error } = await supabase
-    .from("Products")
-    .select(
-      `
-      id, 
-      name, 
-      category,
-      Audiobooks ( * ),
-      Ebooks ( * ),
-      PrintedBooks ( *, 
-        options:PrintOptions ( *, 
-          size:PrintSize( * )
-        ),
-        cover:PrintedCover( * )
-      ),
-      Awards ( * ) 
-      `
-    )
-    .eq("id", params.id)
-    .single();
-
-  if (error) {
-    throw new Error(error);
-  }
-
   return {
     props: {
-      product,
+      params,
     },
   };
 }
 
-export default function ProductPage({ product }) {
-  // console.log(product);
+export default function ProductPage({ params }) {
+  const [session, setSession] = useState(null);
+  const [products, setProducts] = useState(null);
+  const [user, setUser] = useState(null);
+
+  const get_session = async () => {
+    try {
+      const { data, error } = await supabase.auth.getSession();
+      if (error) {
+        console.error(error);
+      } else {
+        setSession(data.session);
+        setUser(data.session.user);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const get_products = async () => {
+    try {
+      const { data: products, error } = await supabase
+        .from("Products")
+        .select(
+          `
+        id,
+        name,
+        category,
+        price,
+        discount,
+        sold,
+        isFeatured,
+        isPublished,
+        releaseDate,
+        publishDate,
+        Audiobooks ( * ),
+        Ebooks ( * ),
+        PrintedBooks ( *,
+          options:PrintOptions ( *,
+            size:PrintSize( * )
+          ),
+          cover:PrintedCover( * )
+        ),
+        ProductsAwards ( *,  awards: Awards(*) )
+        `
+        )
+        .eq("id", params.id);
+      // .eq("name", "Awesome Title");
+
+      if (error) {
+        console.error(error);
+      } else {
+        setProducts(products);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    get_session();
+    get_products();
+  }, []);
+
+  // console.log(products);
   return (
     <div className={styles.container}>
-      <h1> {product.name} </h1>
-      <p> {product.category} </p>
-      <p> {product.price} </p>
-      <pre> {JSON.stringify(product, null, 2)} </pre>
+      {session ? (
+        <div className={styles.container}>
+          <p> logged in as {user.email} </p>
+          <p> {products && JSON.stringify(products, null, 2)} </p>
+        </div>
+      ) : (
+        <div> No User Session </div>
+      )}
       <div className={styles.container}>
         <Link href="/login">Login</Link>
         <Link href="/awards">Add / Remove Awards</Link>
