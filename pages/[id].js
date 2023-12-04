a12c16e3ae6ab945def512a07af19d00c7910c0c
mildfire <mildfire@gmail.com>
2023-12-04 21:54:37 +0500

    fix

--- pages/[id].js
+++ pages/[id].js
@@ -1,6 +1,5 @@
 import supabase from "@/utils/supabase";
 import styles from "../styles/Home.module.css";
-// import Link from "next/link";
 import { useState, useEffect } from "react";
 import Nav from "@/components/nav";
 
